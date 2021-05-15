1da910d796db0b49f24979f29d4ae821a79fd823
AbhinavXT <abhinavpathaka17@gmail.com>
2021-05-15 17:50:45 +0530

    Added Frontend Backend and database using MongoDB

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+  {
+    name: 'Admin',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'User1',
+    email: 'User1@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'User2',
+    email: 'User2@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+]
+
+export default users
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import {Container, Row, Col} from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className="py-3 text-center">&copy; 2021</Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
