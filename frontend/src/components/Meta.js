3f6b2c877328db4d2b3d52f2accf2b5cefab783d
AbhinavXT <abhinavpathaka17@gmail.com>
2021-05-23 15:38:01 +0530

    Brew Buy

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -11,7 +11,7 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-	title: 'Welcome To EShop',
+	title: 'Welcome To Brew Buy',
 	description: 'We sell the best products for cheap',
 	keywords: 'electronics, buy electronics, cheap electroincs',
 }
